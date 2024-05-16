decd071e7437e8b183bc85a8a53605657a3a3453
Abhilash-D-J <jabhilash0311@gmail.com>
2024-05-16 14:44:40 +0530

    Assignment 1 Question 2 Menu Driven Four Function Calculator Program

---
+++ Assignments/Assi1Que3/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Assi1Que3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            int Choice;
+            do
+            {
+                Console.WriteLine("1.Addition \n2.Substraction \n3.Multiplication \n4.Division");
+
+                Console.WriteLine("Enter Choice : ");
+                String kvalue = Console.ReadLine();
+                Choice = Convert.ToInt32(kvalue);
+
+                Console.WriteLine("Enter 1st Number : ");
+                String ivalue = Console.ReadLine();
+                int i = Convert.ToInt32(ivalue);
+
+                Console.WriteLine("Enter 2st Number : ");
+                String jvalue = Console.ReadLine();
+                int j = Convert.ToInt32(jvalue);
+
+                int result;
+
+                switch (Choice)
+                {
+
+                    case 1:
+                        result = i + j;
+                        Console.WriteLine("Result : "+result);
+                        break;
+
+                    case 2:
+                        result = i - j;
+                        Console.WriteLine("Result : " + result);
+                        break;
+
+                    case 3:
+                        result = i * j;
+                        Console.WriteLine("Result : " + result);
+                        break;
+
+                    case 4:
+                        result = i / j;
+                        Console.WriteLine("Result : " + result);
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter valid CHoice !! ");
+                        break;
+                }
+
+            } while (Choice != 0);
+        }
+    }
+}
