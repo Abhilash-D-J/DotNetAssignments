9e9f96e46eeb40863cc7b1488a1faa6d557694f9
Abhilash-D-J <jabhilash0311@gmail.com>
2024-05-28 15:18:29 +0530

    Question 4 Student class

---
+++ Assignments/Question4/Date.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assi2Que1
+{
+    public class Date
+    {
+        private int _day;
+        private int _month;
+        private int _year;
+
+        public Date()
+        {
+
+        }
+
+        public Date(int day, int month, int year)
+        {
+            _day = day;
+            _month = month;
+            _year = year;
+        }
+
+        public int year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+
+
+        public int month
+        {
+            get { return _month; }
+            set { _month = value; }
+        }
+
+
+        public int day
+        {
+            get { return _day; }
+            set { _day = value; }
+        }
+
+        public override string ToString()
+        {
+            return day + "/" + month + "/" + year;
+        }
+
+        public void accept()
+        {
+            Console.WriteLine("Enter day : ");
+            this.day = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter month : ");
+            this.month = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter year : ");
+            this.year = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void print()
+        {
+            Console.WriteLine("date : " + day + "/" + month + "/" + year);
+        }
+
+        public bool isValid()
+        {
+            try
+            {
+                DateTime d = new DateTime(year, month, day);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        public static int dateDiff(Date d1, Date d2)
+        {
+            if (d1.year > d2.year)
+            {
+                return d1.year - d2.year;
+            }
+            else if (d1.year < d2.year)
+            {
+                return d2.year - d1.year;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public static int operator -(Date d1, Date d2)
+        {
+            if (d1.year > d2.year)
+            {
+                return d1.year - d2.year;
+            }
+            else if (d1.year < d2.year)
+            {
+                return d2.year - d1.year;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Assignments/Question4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Assi2Que1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date d1 = new Date();
+            Date d2 = new Date();
+            d1.accept();
+            d2.accept();
+            if(d1.isValid()&& d2.isValid()) {
+                Console.WriteLine("-------dates Are -----");
+                d1.print();
+                d2.print();
+                Console.WriteLine("Date Difference : " + Date.dateDiff(d1, d2));
+                int diff = d1 - d2;
+                Console.WriteLine("Date Difference with Operator Overloading : " + diff);
+            }
+            else
+            {
+                Console.WriteLine("dates Are not valid !! Enter valid dates ");
+            }
+           
+        }
+    }
+
+}
