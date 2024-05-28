cd6094f7ef3919ff5950d256ede6865da8ee57dc
Abhilash-D-J <jabhilash0311@gmail.com>
2024-05-28 15:22:25 +0530

    Question 11 wageEmp Class

---
+++ Assignments/Question11/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Assi3Que11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WageEmp wageEmp = new WageEmp();
+            wageEmp.accept();
+            wageEmp.printDetails();
+        }
+    }
+}
---
+++ Assignments/Question11/WageEmp.cs
@@ -0,0 +1,61 @@
+﻿using Assi3Que8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assi3Que11
+{
+    internal class WageEmp : Employee
+    {
+		private int _hours;
+		private int _rate;
+
+		public WageEmp() : base()
+		{
+			flag = false;
+			this.designation = "Wage";
+		}
+
+        public WageEmp(int hrs,int rate) : base()
+        {
+			flag = false;
+			this._hours = hrs;
+			this._rate = rate;
+			this.designation = "Wage";
+        }
+        public int rate
+		{
+			get { return _rate; }
+			set { _rate = value; }
+		}
+
+		public int hours
+		{
+			get { return _hours; }
+			set { _hours = value; }
+		}
+
+		public void accept()
+		{
+			base.accept();
+			Console.WriteLine("Enter No of Hours : ");
+			this.hours = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Enter Rate : ");
+			this.rate = Convert.ToInt32(Console.ReadLine());
+			this.salary = this.hours*this.rate;
+		}
+
+		public void printDetails()
+		{
+			base .printDetails();
+		}
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
