a6418d47246be00c79f006f909ac981a8a286a06
Abhilash-D-J <jabhilash0311@gmail.com>
2024-05-28 15:21:26 +0530

    Question 9 Manager Class

---
+++ Assignments/Question9/Manager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assi3Que8;
+
+
+namespace Assi3Que9
+{
+    public class Manager : Employee
+    {
+		private double _bonus;
+
+		public Manager() 
+		{
+			this.designation = "Manager";
+		}
+
+        public Manager(double bonus):base()
+        {
+            this.designation = "Manager";
+			this.bonus = bonus;
+        }
+
+        public double bonus
+		{
+			get { return _bonus; }
+			set { _bonus = value; }
+		}
+
+		public void accept()
+		{
+			base.accept();
+			Console.WriteLine("Enter Bonus : ");
+			this.bonus = Convert.ToDouble(Console.ReadLine());
+		}
+
+		public void printDetails()
+		{
+			base.printDetails();
+			Console.WriteLine("Bonus : "+this.bonus);
+		}
+
+        public override string ToString()
+        {
+            return base.ToString()+", Bonus : "+this.bonus;
+        }
+
+    }
+}
---
+++ Assignments/Question9/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assi3Que9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Manager m = new Manager();
+            m.accept();
+            m.printDetails();
+            Console.ReadLine();
+        }
+    }
+}
