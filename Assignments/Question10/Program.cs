6f78a934d692b2d7e6bd17281be5edc9b51f6c24
Abhilash-D-J <jabhilash0311@gmail.com>
2024-05-28 15:21:55 +0530

    Question 10 Supervisor Class

---
+++ Assignments/Question10/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Assi3Que10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Supervisor supervisor = new Supervisor();
+            supervisor.accept();
+            supervisor.printDetails();
+        }
+    }
+}
---
+++ Assignments/Question10/Supervisor.cs
@@ -0,0 +1,46 @@
+﻿using Assi3Que8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assi3Que10
+{
+    internal class Supervisor : Employee
+    {
+		private int _subordinates;
+        
+		public Supervisor() : base()
+		{
+			designation = "Supervisor";
+		}
+        public Supervisor(int subordinates) : base()
+        {
+            designation = "Supervisor";
+			this._subordinates = subordinates;
+        }
+        public int subordinates
+		{
+			get { return _subordinates; }
+			set { _subordinates = value; }
+		}
+		public void accept()
+		{
+			base.accept();
+			Console.WriteLine("Enter Number of Subordinates : ");
+			this.subordinates = Convert.ToInt32(Console.ReadLine());
+		}
+
+		public void printDetails()
+		{
+			base.printDetails();
+			Console.WriteLine("No of Subordinates : "+this.subordinates);
+		}
+
+        public override string ToString()
+        {
+            return base.ToString()+", Subordinates ="+subordinates;
+        }
+    }
+}
